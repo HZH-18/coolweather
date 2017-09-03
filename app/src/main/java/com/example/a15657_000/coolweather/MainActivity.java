0e1a9f2238bd0357fd3d4a170cc8351e39e726fd
Terry <1565769871@qq.com>
2017-09-03 09:14:26 +0800

    Frist commit

---
+++ app/src/main/java/com/example/a15657_000/coolweather/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.a15657_000.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
